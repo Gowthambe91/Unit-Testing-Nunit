f7aee533016fb7fa5495ae735633b37a72a52738
Gowtham <Gowthambe91@gmail.com>
2019-09-10 08:59:36 +0530

    Employee added - 10/09/2019

---
+++ CustomerOrderSvc1/CustomerOrderSvc1/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerOrderSvc1
+{
+    public abstract class Employee
+    {
+        public string Name { get; set; }
+
+        public bool ContainsIllegalChars()
+        {
+            if (this.Name.Contains("$"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public abstract Employee CreateEmployee();
+    }
+
+    public class Manager : Employee
+    {
+        public override Employee CreateEmployee()
+        {
+            return new Manager();
+        }
+    }
+
+    public class DeliveryManager : Employee
+    {
+        public override Employee CreateEmployee()
+        {
+            return new DeliveryManager();
+        }
+    }
+}
