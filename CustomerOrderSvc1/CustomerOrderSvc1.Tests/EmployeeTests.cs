a1ac0c2583a2fd03bb0f96bc74d84a329f630a23
Gowtham <Gowthambe91@gmail.com>
2019-09-10 07:58:59 +0530

    EmployeeTests.cs added -10/09/2019

---
+++ CustomerOrderSvc1/CustomerOrderSvc1.Tests/EmployeeTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomerOrderSvc1;
+using NUnit.Framework;
+
+namespace CustomerOrderSvc1.Tests
+{
+    public class EmployeeTests
+    {
+        [Test]
+        public void When_ContainsIllegalChar_Expected_True() {
+
+            //Arrange
+            Employee employee = new Manager();
+
+            employee.Name = "Gow$tham";
+
+            //Act
+            bool result = employee.ContainsIllegalChars();
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+    }
+
+    public class ManagerTests : EmployeeTests
+    {
+        
+    }
+}
